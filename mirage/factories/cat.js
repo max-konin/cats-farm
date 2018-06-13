dfe3192cb29f27739f72ee8a60c0255a0b278473
Max Konin <maxim21214@gmail.com>
2018-06-13 17:35:57 +0700

    cute coef

--- mirage/factories/cat.js
+++ mirage/factories/cat.js
@@ -1,11 +1,11 @@
 import { Factory, faker, association } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  weight() { return faker.random.number(); },
-  height() { return faker.random.number(); },
-  width() { return faker.random.number(); },
+  weight() { return faker.random.number({min: 1, max: 25}); },
+  height() { return faker.random.number({min: 1, max: 100}); },
+  width() { return faker.random.number({min: 1, max: 100}); },
   name() { return faker.name.firstName(); },
-  fluffiness() { return faker.random.number(); },
+  fluffiness() { return faker.random.number({min: 0, max: 100}); },
   avatarUrl() { return faker.image.cats(100, 100, true); },
   state: faker.list.random('hungry', 'evil', 'sleep'),
   slave: association()
